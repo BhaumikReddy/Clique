52480fbf6d8f3df9434ee9ca223d44f92dda94fc
BhaumikReddy <saibhaumikreddy@gmail.com>
2024-03-20 20:26:30 +0530

    initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import ImageBoard from "./Components/ImageBoard"
+import Navbar from "./Components/Navbar"
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <ImageBoard/>
+    </div>
+  )
+}
+
+export default App
